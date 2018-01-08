dff83a8568bf7c19a0a3dcf44028369ae85b1fc6
DESKTOP-7JBGDJE\yanzi <981352463@qq.com>
2018-01-08 15:31:28 +0800

    注释glide配置

--- photopickerdemo/src/main/java/me/iwf/PhotoPickerDemo/PhotoAdapter.java
+++ photopickerdemo/src/main/java/me/iwf/PhotoPickerDemo/PhotoAdapter.java
@@ -63,9 +63,9 @@ public class PhotoAdapter extends RecyclerView.Adapter<PhotoAdapter.PhotoViewHol
         Glide.with(mContext)
                 .load(uri)
                 .thumbnail(0.1f)
-                .centerCrop()
-                .placeholder(R.drawable.__picker_ic_photo_black_48dp)
-                .error(R.drawable.__picker_ic_broken_image_black_48dp)
+//                .centerCrop()
+//                .placeholder(R.drawable.__picker_ic_photo_black_48dp)
+//                .error(R.drawable.__picker_ic_broken_image_black_48dp)
                 .into(holder.ivPhoto);
       }
     }
